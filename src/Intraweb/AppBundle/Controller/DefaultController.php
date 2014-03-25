57c96623dbc206b5adbc4d7bbc578a305b0dfb69
k-bruneau <bruneaukevin@gmail.com>
2014-03-25 03:30:41 +0100

    ajout homepage

--- src/Intraweb/AppBundle/Controller/DefaultController.php
+++ src/Intraweb/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    // should be homepage
     public function indexAction()
     {
         return $this->render('IntrawebAppBundle:Default:index.html.twig');
